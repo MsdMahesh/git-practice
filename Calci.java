beff8a6ec7f7d5a7216acb56e94ae9ccaa20f2f7
MsdMahesh <engineertest150@gmail.com>
2024-01-11 12:57:51 +0530

    added

--- Calci.java
+++ Calci.java
@@ -3,3 +3,14 @@ Interface Calci
  public int getSum(int a,int b);
  public int getDiff(int a,int b);
 }
+public class CalciImple implements Calci
+{
+  public int getSum(int a,int b)
+	{
+	  return a+b;
+	}
+	public int getDiff(int a,int b)
+	{
+	  return a-b;
+	}
+}
